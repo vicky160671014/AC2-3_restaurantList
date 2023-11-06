cc8ad41ed01ac5d0138685ea6f6c1265925be66b
vicky160671014 <vicky160671014@gmail.com>
2023-11-06 15:49:43 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 //Include express from node_modules and define server related variables
 const express = require('express')
+const mongoose = require('mongoose')
+
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
+
 const app = express()
 const port = 3000
+
+//MONGODB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error',()=>{
+  console.log('mongodb error!')
+})
+db.once('open',()=>{
+  console.log('mongodb connected!')
+})
 
 //require express-handlebars here
 const exphbs = require('express-handlebars')
---
+++ modules/restaurant.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const restaurantSchema = mongoose.Schema({
+  name:{ type: String , required: true },
+  name_en: { type: String, required: true },
+  category: { type: String, required: true },
+  image: { type: String, required: true },
+  location: { type: String, required: true },
+  phone: { type: String, required: true },
+  google_map: { type: String, required: true },
+  rating: { type: Number, required: true },
+  description: { type: String, required: true },
+})
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
